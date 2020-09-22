5f9c6a36dd2fa5553d3a7b2ea5777b73dcdd1da2
jarbas.282505 <jarbaspin@gmail.com>
2020-09-22 15:29:24 -0300

    Adição do construtor da classe animal

--- src/SistemaAnimal/Animal.java
+++ src/SistemaAnimal/Animal.java
@@ -2,15 +2,14 @@ package SistemaAnimal;
 
 public class Animal {
     private int idade;
+    private int quantidadeDeMembros;
+    private double peso;
 
     public Animal(int idade, int quantidadeDeMembros, double peso) {
         this.idade = idade;
         this.quantidadeDeMembros = quantidadeDeMembros;
         this.peso = peso;
     }
-
-    private int quantidadeDeMembros;
-    private double peso;
 
 //    public void movimentoAnimal(){
 //
