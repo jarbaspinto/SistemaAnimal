79de29bc40f46e359d1282766efe21f8b1bf513c
jarbas.282505 <jarbaspin@gmail.com>
2020-09-22 15:30:26 -0300

    Second commit - create mamifero class

--- src/SistemaAnimal/Animal.java
+++ src/SistemaAnimal/Animal.java
@@ -11,6 +11,7 @@ public class Animal {
         this.peso = peso;
     }
 
+
 //    public void movimentoAnimal(){
 //
 //    }
--- src/SistemaAnimal/Mamifero.java
+++ src/SistemaAnimal/Mamifero.java
@@ -3,8 +3,4 @@ package SistemaAnimal;
 public class Mamifero extends Animal {
     private String corDoPelo;
 
-    @Override
-    public String emitirSom() {
-        return super.emitirSom();
-    }
 }
