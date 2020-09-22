6d2be4b619cc3657673157a373f8cc693e88f8d3
jarbas.282505 <jarbaspin@gmail.com>
2020-09-22 15:05:10 -0300

    First commit - create animal class

---
+++ src/SistemaAnimal/Animal.java
@@ -0,0 +1,14 @@
+package SistemaAnimal;
+
+public class Animal {
+    private int idade;
+    private int quantidadeDeMembros;
+    private double peso;
+
+//    public void movimentoAnimal(){
+//
+//    }
+//    public String emitirSom(){
+//        return emitirSom();
+//    }
+}
